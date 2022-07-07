552a5fe6a02e1b6b8a2ee93dab2692805c4d56d5
Steve Ross <steveross1975@gmail.com>
2022-07-07 17:17:40 +0200

    Tested Django Admin Console

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -26,7 +26,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """Create a new Superuser with given detaile"""
-        user = self.create_user(name, email, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
