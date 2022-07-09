397e47a5326f2a15696e6f87b33afe1d42a02751
Steve Ross <steveross1975@gmail.com>
2022-07-09 15:57:35 +0200

    Added User Permissions of Feeds

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -15,3 +15,19 @@ class UpdateOwnProfile(permissions.BasePermission):
         # Boolean Return True if user editing
         # is trying to edit his own profile
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allows users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update his own status"""
+
+        if request.method in permissions.SAFE_METHODS:
+            # SAFE_METHODS = List objects and create new objects
+            # We want to allow users to see
+            # the profile of other users
+            return True
+
+        # Boolean Return True if user editing
+        # is trying to edit his own profile
+        return obj.user_profile.id == request.user.id        
