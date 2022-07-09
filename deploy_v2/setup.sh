7c41ed03095b2b39537ea7f8e92798dabb559785
Steve Ross <steveross1975@gmail.com>
2022-07-09 17:07:58 +0200

    changed directory

--- deploy_v2/setup.sh
+++ deploy_v2/setup.sh
@@ -22,7 +22,7 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/profile-rest-api
 mkdir -p $VIRTUALENV_BASE_PATH
 python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
 
-$VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
+$VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profile-rest-api/requirements.txt
 
 # Run migrations
 cd $PROJECT_BASE_PATH/profile-rest-api/src
